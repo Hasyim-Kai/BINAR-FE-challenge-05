361413eb844eebeff14feb45bb0e875f8df2a021
Kai-69 <ichihatsu1@gmail.com>
2022-04-19 06:32:28 +0700

    Refactor : Edit Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store";
 import AppRoutes from "./Routes";
 
 export default function App() {
-  return <Provider store={store}><AppRoutes /></Provider>
+  return <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <AppRoutes />
+    </PersistGate>
+  </Provider>
 }
